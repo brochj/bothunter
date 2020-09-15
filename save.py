d41069d23ea1b4e007121b98075d67cdcf9e685d
brochj <brochj@gmail.com>
2020-09-15 17:01:04 -0300

    Added account verification

--- save.py
+++ save.py
@@ -14,14 +14,28 @@ import os
 class Result:
     
     def __init__(self):
-        
        self.__create_save_dir() 
 
     def __create_save_dir(self):
         if not os.path.exists("results"):
             os.makedirs("results/")
     
-    def save_account(self, account):
-        file = open('results/accounts.txt', 'a')
-        file.write(f'{account}\n')
+    
+    def __verify_added_accounts(self, account):
+        file = open('results/accounts.txt', 'r')
+        names_list = file.readlines()
         file.close()
+        for name in names_list:
+            if account == name.strip():    
+                return True
+
+        
+    def save_account(self, account):
+        if not self.__verify_added_accounts(account):
+            file = open('results/accounts.txt', 'a')
+            file.write(f'{account}\n')
+            file.close()
+        else:
+            print(f'@{account} - This account  has already been verified and added to the list')
+    
+    
